192bc9a711826fa9de09bb6c49ccd9c379b8f213
Michał Tamaka <tamahawk@wp.pl>
2022-11-24 10:47:58 +0100

    correcting function in JS

--- js/script.js
+++ js/script.js
@@ -5,43 +5,42 @@
 
     welcome();
 
-    {
-        const toggleBackground = () => {
-            const buttonThree = document.querySelector(".js-buttonThree");
-            const body = document.querySelector(".js-body");
-            const themeName = document.querySelector(".js-themeName");
-            buttonThree.addEventListener("click", toggleBackground);
+    const toggleBackground = () => {
+        const buttonThree = document.querySelector(".js-buttonThree");
+        const body = document.querySelector(".js-body");
+        const themeName = document.querySelector(".js-themeName");
+
+        buttonThree.addEventListener("click", () => {
             body.classList.toggle("dark");
             themeName.innerText = body.classList.contains("dark") ? "light" : "dark";
-        };
-
-        toggleBackground();
+        });
     };
-    {
-        const headerRemove = () => {
-            const buttonOne = document.querySelector(".js-buttonOne");
-            const header = document.querySelector(".js-header");
-            buttonOne.addEventListener("click", () => {
 
-                header.remove()
-                buttonOne.remove()
+    toggleBackground();
 
-            });
-        };
+    const headerRemove = () => {
+        const buttonOne = document.querySelector(".js-buttonOne");
+        const header = document.querySelector(".js-header");
+        buttonOne.addEventListener("click", () => {
 
-        headerRemove();
+            header.remove()
+            buttonOne.remove()
+
+        });
     };
-    {
-        const portraitRemove = () => {
-            const buttonTwo = document.querySelector(".js-buttonTwo");
-            const picture = document.querySelector(".js-picture");
-            buttonTwo.addEventListener("click", () => {
 
-                picture.remove()
-                buttonTwo.remove()
-            });
-        };
+    headerRemove();
 
-        portraitRemove();
+    const portraitRemove = () => {
+        const buttonTwo = document.querySelector(".js-buttonTwo");
+        const picture = document.querySelector(".js-picture");
+        buttonTwo.addEventListener("click", () => {
+
+            picture.remove()
+            buttonTwo.remove()
+        });
     };
+
+    portraitRemove();
+
 };
